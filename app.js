2612089fa7d8c133244495a5949af2bda7b1f05b
Lucas Santos <santoslukz@gmail.com>
2025-08-03 19:10:18 -0300

    finalizei o crud de postagens, criei a pagina inicial do blog

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const path = require('path')
 const session = require('express-session')
 const flash = require('connect-flash')
 const moment = require('moment')
+require('./models/Postagem')
+const Postagem =  mongoose.model('postagens')
+require('./models/Categoria')
+const Categoria = mongoose.model('categorias')
 
 //config
 app.use(express.urlencoded({extended:true}))
@@ -55,6 +59,69 @@ app.use(express.json())
     app.use(express.static(path.join(__dirname,"public")))
 
 //rotas
+app.get('/', (req,res)=>{
+    Postagem.find().populate('categoria').sort({data:'desc'}).then((postagens)=>{
+        res.render('index',{postagens: postagens})
+
+    }).catch((err)=>{
+        req.flash('error_msg','Houve um erro', err)
+        res.redirect('/404')
+    })
+})
+
+app.get('/postagem/:slug', (req,res)=>{
+    Postagem.findOne({slug: req.params.slug}).then((postagem)=>{
+        if(postagem){
+            res.render('postagem/index', {postagem: postagem})
+        
+        } else{
+            req.flash('error_msg','Essa postagem não existe')
+            res.redirect('/')
+        }
+    }).catch((err)=>{
+        req.flash('error_msg', 'houve um erro interno', err)
+        res.redirect('/')
+    })
+})
+
+app.get('/categorias', (req,res)=>{
+    Categoria.find().then((categorias)=>{
+        res.render('categoria/index',{categorias: categorias})
+    }).catch((err) =>{
+        req.flash('error_msg', 'Erro ao listar categorias')
+        res.redirect('/')
+    })
+})
+
+
+// Rota para exibir postagens de uma categoria específica pelo slug
+app.get('/categorias/:slug', (req, res) => {
+    // Busca a categoria pelo slug passado na URL
+    Categoria.findOne({slug: req.params.slug }).then((categoria) => {
+        if (categoria) {
+            // Se encontrar a categoria, busca todas as postagens relacionadas a ela
+            Postagem.find({ categoria: categoria._id }).then((postagens) => {
+                // Renderiza a página mostrando as postagens da categoria
+                res.render('categoria/postagens', { postagens: postagens, categoria: categoria })
+            }).catch((err) => {
+                // Se ocorrer erro ao buscar postagens, exibe mensagem e redireciona
+                req.flash('error_msg', 'Erro ao listar postagem!')
+                res.redirect('/')
+            })
+        } else {
+            // Se não encontrar a categoria, exibe mensagem e redireciona
+            req.flash('error_msg', 'Essa categoria não existe!')
+            res.redirect('/')
+        }
+    })
+})
+
+app.get('/404',(req,res)=>{
+    res.send('Erro 404!')
+})
+
+
+
 app.use('/admin', admin)
 
 //outros
